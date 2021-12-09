6ebd770bf499efe028afab636f046140cb15c00b
Aaditya Shah <0blvn@protonmail.com>
2021-12-09 13:19:51 +0530

    added error handling

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var roomtext = document.querySelector('#roomid');
 
-roomtext.addEventListener('click', () => {
-    console.log(roomtext.value);
+function handle(e) {
+    e.preventDefault()
+}
+
+roomtext.addEventListener('change', () => {
+    if (roomtext.value !== 'CX10019AB') {
+        var joinpage = document.querySelector('.joinlink');
+        var x = joinpage.addEventListener('click', handle);
+        roomtext.style.background = '#ff2222';
+
+        alert('Please enter correct room code');
+    } else {
+        var joinpage = document.querySelector('.joinlink');
+        joinpage.removeEventListener('click', handle);
+        roomtext.style.background = '#52ff00';
+    }
 });
